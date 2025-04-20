75a134369cbba559db13fe918380a6b2ad952ec9
demoon <wibawa633@gmail.com>
2025-04-20 20:33:53 +0700

    update db dan model

--- be/config/db.js
+++ be/config/db.js
@@ -1,9 +1,8 @@
 import { Sequelize } from "sequelize";
 
-// Nyambungin db ke BE
-const db = new Sequelize("havas", "root", "", {
-  host: "104.197.117.136",
-  dialect: "mysql",
+const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+    host: "104.197.117.136",
+    dialect: "mysql",
 });
 
 export default db;
